8e34d1bcdf44de10b801ab8b3ce6ddfb5f291e3b
MaddieVorhies <maddie_vorhies1@baylor.edu>
2020-10-20 20:30:41 -0500

    Lab08

--- Lab08/systemverilog/sseg4.sv
+++ Lab08/systemverilog/sseg4.sv
@@ -34,6 +34,8 @@ module sseg4(
     wire [15:0] out_mux2;
     wire [3:0] in;
     wire [6:0] in0_1;
+    reg [6:0] in1_1;
+    wire and_out;
     
     bcd11 bcd (
        .B(data[10:0]),
@@ -43,13 +45,42 @@ module sseg4(
        .thousands(in0_mux2[15:12])
     );
     
-    mux2 mux_0 (
+    mux2 mux2_0 (
        .in1(data[15:0]), 
        .in0({thousands, hundreds, tens, ones}),
-       .out(out_mux2[15:0])
+       .out(out_mux2[15:0]),
+       .sel(hex_dec)
+   );
+   
+   mux4 mux4_0 (
+      .sel(digit_sel[1:0]),
+      .out(in[3:0]),
+      .in3(out_mux2[15:12]),
+      .in2(out_mux2[11:8]),
+      .in1(out_mux2[7:4]),
+      .in0(out_mux2[3:0])
+   );
+   
+   sseg_decoder sseg_dec ( 
+      .num(in[3:0]), 
+      .sseg(in0_1[6:0])
+   );
+   
+   mux2 mux2_1 (
+      .in1(in1_1[6:0]), 
+      .in0(in0_1[6:0]), 
+      .sel(and_out),
+      .out(seg[6:0])
+   );
+   
+   an_decoder an_dec (
+      .in(digit_sel[1:0]), 
+      .out(an[3:0])
    );
     
-       
+   assign and_out = sign & ~an[3];
+   assign dp = 1'b1;
+   assign in1_1[6:0] = 7'b0111111;
    
     
     
---
+++ Lab08/systemverilog/sseg4_manual.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/20/2020 08:21:39 PM
+// Design Name: 
+// Module Name: sseg4_manual
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sseg4_manual(
+    input [15:0] sw,
+    output [6:0] seg,
+    output dp,
+    output [3:0] an
+    );
+endmodule
